504fdf6bed89b58112d58a2969d58aa9b34cae35
SealyPrograming <lafar@DRAK01.(none)>
2025-01-18 17:53:37 +0100

    made main.py the main file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
 from RandomForest import RandomForest
+from test import test
 
 # Przykład użycia:
 X = [[2.8, 1.0], [3.4, 2.1], [1.2, 0.5], [3.0, 1.5], [1.5, 0.7]]
@@ -11,3 +14,8 @@ forest.fit(X, y)
 # Predykcja
 predictions = forest.predict(X)
 print(predictions)
+
+# Generowanie danych
+X, y = make_classification(n_samples=1000, n_features=20, n_classes=2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+test(X_train, X_test, y_train, y_test)
